5f53a48df2bbb38ecd9b58bbb8ec59c570262c42
4000D <dkdkajej@gmail.com>
2018-01-25 14:22:19 +0900

    refactor test data

--- test/schema.js
+++ test/schema.js
@@ -1,4 +1,6 @@
+import path from "path";
 import BigNumber from "bignumber.js";
+
 import schema from "../src/index";
 
 const should = require("chai")
@@ -6,9 +8,11 @@ const should = require("chai")
   .use(require("chai-bignumber")(BigNumber))
   .should();
 
+const dataDir = path.resolve(__dirname, "./tokyo-test-data");
+
 describe("Input Schema", () => {
   it("sample data 1", () => {
-    const data = require("./data/sample1.json");
+    const data = require(path.resolve(dataDir, "./sample1.json"));
     const { error } = schema.validate(data);
 
     should.not.exist(error);
