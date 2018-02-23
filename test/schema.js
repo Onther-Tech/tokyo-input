867b9a87426b64d67ff378b68a28950922913616
4000D <dkdkajej@gmail.com>
2018-02-23 13:54:53 +0900

    refactor submodules

--- test/schema.js
+++ test/schema.js
@@ -8,11 +8,9 @@ const should = require("chai")
   .use(require("chai-bignumber")(BigNumber))
   .should();
 
-const dataDir = path.resolve(__dirname, "./tokyo-test-data");
-
 describe("Input Schema", () => {
   it("sample data 1", () => {
-    const data = require(path.resolve(dataDir, "./sample1.json"));
+    const data = require("tokyo-test-data/sample1.json");
     const { error } = schema.validate(data);
 
     should.not.exist(error);
